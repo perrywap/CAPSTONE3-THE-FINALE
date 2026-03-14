3a5ae1c184399d384ee1fbd0828f9b10f826b6ba
Gerald <geraldperry.reyes@benilde.edu.ph>
2026-03-15 03:42:35 +0800

    commit

--- Plastic Man/Assets/Scripts/NerfGun.cs
+++ Plastic Man/Assets/Scripts/NerfGun.cs
@@ -7,13 +7,14 @@ public class NerfGun : WeaponBase
 
     protected override void Fire(Vector3 origin, Vector3 direction)
     {
-        CameraShake.Instance.Shake();
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         Quaternion rotation = Quaternion.Euler(0f, 0f, angle);
 
         GameObject bullet = Instantiate(projectilePrefab, muzzle.position, muzzle.rotation);
 
         Projectile2D projectile = bullet.GetComponent<Projectile2D>();
+        projectile.damage = damage;
+
         if (projectile != null)
         {
             projectile.Launch(direction);
