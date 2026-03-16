14c6c6ed086857d59fc28c32f37cf2b6ea342c3e
Gerald Perry Reyes <geraldperry.reyes@gmail.com>
2026-03-16 17:23:47 +0800

    fixed weapons, added muzzle flash and vfx

--- Plastic Man/Assets/Scripts/Projectile.cs
+++ Plastic Man/Assets/Scripts/Projectile.cs
@@ -2,10 +2,12 @@ using UnityEngine;
 
 public class Projectile2D : MonoBehaviour
 {
+    [Header("References")]
+    [SerializeField] private GameObject vfx;
+
     [Header("Movement")]
     public float speed = 10f;
     public float lifetime = 3f;
-
 
     [Header("Damage")]
     public float damage;
@@ -29,6 +31,13 @@ public class Projectile2D : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D collision)
     {
-        Destroy(gameObject);
+        EnemyAI enemy = collision.GetComponent<EnemyAI>();
+
+        if (enemy != null)
+        {
+            Instantiate(vfx, this.transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+        
     }
 }
